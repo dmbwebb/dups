1a398afbab43dd4780ca092adff5b23dd75c95fd
dmbwebb <dmbwebb@gmail.com>
2020-08-20 13:05:07 +0200

    added dups_stop

--- R/dups_warn.R
+++ R/dups_warn.R
@@ -12,4 +12,25 @@
 dups_warn <- function(data, ...) {
   n_dups <- dups_count(data, ...)
   if (n_dups > 0) warning(paste0("There are ", n_dups, " non-unique rows in the dataset"))
+
+  invisible(data)
+}
+
+
+#' Stop if there are duplicates in a dataset
+#'
+#' For use within functions, stops operations and throws an error if there are any duplicates in the data
+#'
+#' @param data data.frame or tibble
+#' @param ... id variables
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dups_stop <- function(data, ...) {
+  n_dups <- dups_count(data, ...)
+  if (n_dups > 0) stop(paste0("There are ", n_dups, " non-unique rows in the dataset"))
+
+  invisible(data)
 }
